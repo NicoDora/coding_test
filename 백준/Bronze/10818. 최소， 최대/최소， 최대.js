f715d0726cc5b22156a0b280871d3600f741cfce
NicoDora <pjh_2004@naver.com>
2024-10-24 20:45:42 +0900

    [Bronze III] Title: 최소, 최대, Time: 404 ms, Memory: 89964 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,14 +1,6 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const count = Number(input[0]);
-const numbers = input[1].split(" ").map(Number);
-let min = 1000000;
-let max = -1000000;
+const numbers = input[1].split(" ");
 
-for (let i = 0; i < count; i++) {
-  if (numbers[i] < min) min = numbers[i];
-  if (numbers[i] > max) max = numbers[i];
-}
-
-console.log(`${min} ${max}`);
+console.log(Math.min(...numbers), Math.max(...numbers));
