3c5703002ea522873a413d064e44b02b8d0bd31e
NicoDora <pjh_2004@naver.com>
2024-10-29 15:59:01 +0900

    [Bronze II] Title: 벌집, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.js
+++ 백준/Bronze/2292. 벌집/벌집.js
@@ -4,11 +4,8 @@ const input = fs.readFileSync("/dev/stdin").toString().trim();
 let weight = 1;
 let i = 1;
 
-if (Number(input) !== 1) {
-  while (6 * i + weight < Number(input)) {
-    weight += 6 * i;
-    i++;
-  }
+while (Number(input) > weight) {
+  weight += 6 * i;
   i++;
 }
 
