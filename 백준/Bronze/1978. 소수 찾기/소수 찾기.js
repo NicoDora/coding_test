f06e4d5d879bfe2c4c1d704993967649b8638e17
NicoDora <pjh_2004@naver.com>
2024-10-30 17:03:23 +0900

    [Bronze II] Title: 소수 찾기, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.js
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -4,16 +4,20 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const numbers = input[1].split(" ").map(Number);
 let primeCount = 0;
 
-function isPrime(n) {
-  if (n < 2) return 0;
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) return 0;
-  }
-  return 1;
-}
-
 for (let i = 0; i < Number(input[0]); i++) {
-  primeCount += isPrime(numbers[i]);
+  const num = numbers[i];
+  if (num < 2) continue;
+
+  let isPrime = true;
+
+  for (let j = 2; j < num; j++) {
+    if (num % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) primeCount++;
 }
 
 console.log(primeCount);
