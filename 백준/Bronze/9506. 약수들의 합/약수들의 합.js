d0ffcd522695dc1f3eb4e38f50fce74f8b8cb8d9
NicoDora <pjh_2004@naver.com>
2024-10-30 15:33:52 +0900

    [Bronze I] Title: 약수들의 합, Time: 96 ms, Memory: 9720 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.js
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.js
@@ -4,16 +4,22 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 for (let i = 0; i < input.length - 1; i++) {
   let divisor = [];
 
-  for (let j = 0; j < Number(input[i]); j++) {
+  for (let j = 0; j <= Math.sqrt(Number(input[i])); j++) {
     if (Number(input[i]) % j === 0) {
       divisor.push(j);
+      if (j !== Number(input[i]) / j) {
+        divisor.push(Number(input[i]) / j);
+      }
     }
   }
 
-  const isPerfect = divisor.reduce((p, c) => p + c) === Number(input[i]);
+  const isPerfect =
+    divisor.reduce((p, c) => p + c) - Number(input[i]) === Number(input[i]);
 
   if (isPerfect) {
     divisor.sort((a, b) => a - b);
+    divisor.pop();
+      
     console.log(`${Number(input[i])} = ${divisor.join(" + ")}`);
   } else {
     console.log(`${input[i]} is NOT perfect.`);
