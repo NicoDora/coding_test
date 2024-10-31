cebfffee2d596c4ec2e3a43251243543ff80a329
NicoDora <pjh_2004@naver.com>
2024-10-31 20:34:05 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 9616 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -3,16 +3,13 @@ const input = fs.readFileSync("/dev/stdin").toString().trim();
 
 let N = Number(input);
 
-if (N === 2) console.log(2);
-if (N === 3) console.log(3);
-
 for (let i = 2; i * i <= N; ) {
-  while (N !== 1) {
-    if (N % i === 0) {
-      console.log(i);
-      N = N / i;
-    } else {
-      i++;
-    }
+  if (N % i === 0) {
+    console.log(i);
+    N = N / i;
+  } else {
+    i++;
   }
 }
+
+if (N !== 1) console.log(N);
