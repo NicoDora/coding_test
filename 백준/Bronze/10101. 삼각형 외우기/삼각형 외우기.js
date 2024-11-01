ead7a6e8fb407a40a04f86cc9a7717a8f71aae87
NicoDora <pjh_2004@naver.com>
2024-11-01 16:37:41 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const [angle1, angle2, angle3] = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
+
+if (angle1 + angle2 + angle3 === 180) {
+  if (angle1 === angle2 || angle1 === angle3 || angle2 === angle3) {
+    if (angle1 === angle2 && angle2 === angle3 && angle3 === 60) {
+      console.log("Equilateral");
+    } else {
+      console.log("Isosceles");
+    }
+  } else {
+    console.log("Scalene");
+  }
+} else {
+  console.log("Error");
+}
