e2fbc7d997bd8adb85510925d26f0f774294d981
NicoDora <pjh_2004@naver.com>
2024-11-01 13:35:13 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.js
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.js
@@ -1,11 +1,6 @@
 const fs = require("fs");
 const [x, y, w, h] = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
 
-const distance = [x, y];
-
-distance.push(Math.abs(x - w));
-distance.push(Math.abs(y - h));
-
-const result = Math.min(...distance);
+const result = Math.min(x, y, w - x, h - y);
 
 console.log(result);
