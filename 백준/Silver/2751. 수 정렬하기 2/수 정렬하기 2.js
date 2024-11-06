7ee125d0d651b03cafe710c4cc73a8067c83aea3
NicoDora <pjh_2004@naver.com>
2024-11-06 17:49:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1196 ms, Memory: 209724 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
+
+input.shift();
+
+console.log(input.sort((a, b) => a - b).join("\n"));
