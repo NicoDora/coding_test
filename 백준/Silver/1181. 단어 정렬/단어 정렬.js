fd962e9a81eb455d886d635ac49d56af0ba0e412
NicoDora <pjh_2004@naver.com>
2024-11-07 17:49:19 +0900

    [Silver V] Title: 단어 정렬, Time: 220 ms, Memory: 17912 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -2,13 +2,9 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 input.shift();
-let result = "";
 const words = input
   .sort((a, b) => a.length - b.length || a.localeCompare(b))
-  .filter((word, index, array) => index === 0 || word !== array[index - 1]);
+  .filter((word, index, array) => index === 0 || word !== array[index - 1])
+  .join("\n");
 
-for (let i = 0; i < words.length; i++) {
-  result += `${words[i]}\n`;
-}
-
-console.log(result);
+console.log(words);
