c5e4ca7da2d62072c6fc87135cb46e07b8572e7e
NicoDora <pjh_2004@naver.com>
2024-11-07 21:38:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 256 ms, Memory: 30520 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,31 +1,12 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-function merge(array1, array2) {
-  let result = [];
-
-  while (array1.length && array2.length) {
-    if (Number(array1[0][0]) <= Number(array2[0][0])) {
-      result.push(array1.shift());
-    } else {
-      result.push(array2.shift());
-    }
-  }
-
-  return [...result, ...array1, ...array2];
-}
-
-function mergeSort(array) {
-  if (array.length <= 1) return array;
-
-  let mid = Math.floor(array.length / 2);
-  let left = mergeSort(array.slice(0, mid));
-  let right = mergeSort(array.slice(mid));
-
-  return merge(left, right);
-}
-
 const N = Number(input.shift());
-const sortedArray = mergeSort(input.map((e) => e.split(" ")));
+const array = new Array(201).fill("");
 
-sortedArray.forEach(([age, name]) => console.log(`${age} ${name}`));
+for (let i = 0; i < N; i++) {
+  const age = Number(input[i].split(" ")[0]);
+  array[age] += input[i] + "\n";
+}
+
+console.log(array.join(""));
