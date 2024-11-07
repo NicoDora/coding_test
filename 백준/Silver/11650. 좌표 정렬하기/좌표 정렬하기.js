7b6468b127acf7939edbfe3a1711a45c52f4ebde
NicoDora <pjh_2004@naver.com>
2024-11-07 15:21:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 444 ms, Memory: 57956 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -3,9 +3,14 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 input.shift();
 const numbers = input.map((line) => line.split(" ").map(Number));
+let result = "";
 
 numbers.sort((a, b) => {
   return a[0] === b[0] ? a[1] - b[1] : a[0] - b[0];
 });
 
-numbers.forEach((e) => console.log(e.join(" ")));
+for (let i = 0; i < numbers.length; i++) {
+  result += `${numbers[i][0]} ${numbers[i][1]}\n`;
+}
+
+console.log(result);
