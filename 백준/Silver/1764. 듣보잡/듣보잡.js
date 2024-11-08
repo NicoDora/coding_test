f9d043bd3219169ba6c2247a6df396a734a37572
NicoDora <pjh_2004@naver.com>
2024-11-08 19:51:43 +0900

    [Silver IV] Title: 듣보잡, Time: 200 ms, Memory: 25684 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.js
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -2,29 +2,13 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number);
-const set1 = new Set();
-const set2 = new Set();
+const set = new Set(input.slice(0, N));
+const array = input.slice(N);
 let result = [];
 
-for (let i = 0; i < N; i++) {
-  set1.add(input[i]);
-}
-
-for (let i = N; i < N + M; i++) {
-  set2.add(input[i]);
-}
-
-let bigSet = set1;
-let smallSet = set2;
-
-if (set1.size < set2.size) {
-  bigSet = set2;
-  smallSet = set1;
-}
-
-for (let name of smallSet) {
-  if (bigSet.has(name)) result.push(name);
+for (let i = 0; i < M; i++) {
+  if (set.has(array[i])) result.push(array[i]);
 }
 
 console.log(result.length);
-console.log(result.sort((a, b) => (a > b ? 1 : -1)).join("\n"));
+console.log(result.sort().join("\n"));
