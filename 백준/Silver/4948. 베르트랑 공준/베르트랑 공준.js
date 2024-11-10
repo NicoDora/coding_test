8d2e1279a9e31be34db0bc385dbf6ed988ec672e
NicoDora <pjh_2004@naver.com>
2024-11-10 12:48:59 +0900

    [Silver II] Title: 베르트랑 공준, Time: 176 ms, Memory: 11960 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.js
@@ -1,33 +1,26 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
 
-function countPrime(num) {
-  if (num === 1) return 1;
-
-  const array = new Array(num * 2).fill(1);
-  const prime = [];
-
-  for (let i = 2; i <= Math.sqrt(num * 2); i++) {
-    if (array[i]) {
-      for (let j = i * i; j <= num * 2; j += i) {
-        array[j] = 0;
-      }
-    }
-  }
-
-  array[1] = 0;
-
-  for (let i = num + 1; i <= num * 2; i++) {
-    if (array[i]) prime.push(i);
-  }
-
-  return prime.length;
-}
-
+const array = new Array(123456 * 2 + 1).fill(1);
 const result = [];
 
+for (let i = 2; i <= Math.sqrt(array.length); i++) {
+  if (array[i]) {
+    for (let j = i * i; j <= 123456 * 2; j += i) {
+      array[j] = 0;
+    }
+  }
+}
+
 for (let i = 0; i < input.length - 1; i++) {
-  result.push(countPrime(input[i]));
+  const n = input[i];
+  let count = 0;
+
+  for (let j = n + 1; j <= n * 2; j++) {
+    if (array[j]) count++;
+  }
+
+  result.push(count);
 }
 
 console.log(result.join("\n"));
