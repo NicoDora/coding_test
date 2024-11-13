9cd3c95e9d46937fbe8626b4f3f258e09e281bf1
NicoDora <pjh_2004@naver.com>
2024-11-13 21:34:45 +0900

    [Bronze I] Title: 이항 계수 1, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.js
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 const [N, K] = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
 
-function NCK(n, k) {
-  if (k > n) return 0;
-  else if (k === 0 || k === n) return 1;
-  return NCK(n - 1, k) + NCK(n - 1, k - 1);
+function factorial(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 }
 
-console.log(NCK(N, K));
+console.log(factorial(N) / (factorial(K) * factorial(N - K)));
