36d251e35ba036df475d835000056a13413a3883
NicoDora <pjh_2004@naver.com>
2024-11-13 20:45:39 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.js
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const input = Number(fs.readFileSync("/dev/stdin").toString().trim());
 
-function factorial(num) {
-  if (num === 0) return 1;
-  return num * factorial(num - 1);
+let result = 1;
+
+for (let i = 1; i <= input; i++) {
+  result *= i;
 }
 
-console.log(factorial(input));
+console.log(result);
