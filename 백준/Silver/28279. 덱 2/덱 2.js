fc8d29dfb5de0a6bed46cd55dcd7e20dee10056c
NicoDora <pjh_2004@naver.com>
2024-11-13 16:10:55 +0900

    [Silver IV] Title: 덱 2, Time: 2800 ms, Memory: 295992 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.js
+++ 백준/Silver/28279. 덱 2/덱 2.js
@@ -1,38 +1,92 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
+class Deque {
+  constructor() {
+    this.deque = {};
+    this.size = 0;
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  pushFront(num) {
+    this.front--;
+    this.deque[this.front] = num;
+    this.size++;
+  }
+
+  pushBack(num) {
+    this.deque[this.rear] = num;
+    this.rear++;
+    this.size++;
+  }
+
+  popFront() {
+    if (this.isEmpty()) return -1;
+    const popNum = this.deque[this.front];
+    delete this.deque[this.front];
+    this.front++;
+    this.size--;
+    return popNum;
+  }
+
+  popBack() {
+    if (this.isEmpty()) return -1;
+    this.rear--;
+    const popNum = this.deque[this.rear];
+    delete this.deque[this.rear];
+    this.size--;
+    return popNum;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return this.size ? 0 : 1;
+  }
+
+  getFront() {
+    return this.isEmpty() ? -1 : this.deque[this.front];
+  }
+
+  getBack() {
+    return this.isEmpty() ? -1 : this.deque[this.rear - 1];
+  }
+}
+
 function program(cmd, x) {
   switch (cmd) {
     case 1:
-      deck[--startPointer] = x;
+      deque.pushFront(x);
       return null;
     case 2:
-      deck[endPointer++] = x;
+      deque.pushBack(x);
       return null;
     case 3:
-      return startPointer < endPointer ? deck[startPointer++] : -1;
+      return deque.popFront();
     case 4:
-      return startPointer < endPointer ? deck[--endPointer] : -1;
+      return deque.popBack();
     case 5:
-      return endPointer - startPointer;
+      return deque.getSize();
     case 6:
-      return startPointer === endPointer ? 1 : 0;
+      return deque.isEmpty();
     case 7:
-      return startPointer < endPointer ? deck[startPointer] : -1;
+      return deque.getFront();
     case 8:
-      return startPointer < endPointer ? deck[endPointer - 1] : -1;
+      return deque.getBack();
   }
 }
 
 const N = Number(input[0]);
-const deck = [];
+const deque = new Deque();
 const result = [];
-let startPointer = 0;
-let endPointer = 0;
 
 for (let i = 1; i <= N; i++) {
   const [cmd, x] = input[i].split(" ").map(Number);
-  result.push(program(cmd, x));
+  const res = program(cmd, x);
+  if (res !== null) result.push(res);
 }
 
-console.log(result.filter((e) => e !== null).join("\n"));
+console.log(result.join("\n"));
