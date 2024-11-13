3cf6fb19de4838231fcc841daac516da282fd51c
NicoDora <pjh_2004@naver.com>
2024-11-13 17:38:09 +0900

    [Silver III] Title: queuestack, Time: 264 ms, Memory: 48812 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.js
+++ 백준/Silver/24511. queuestack/queuestack.js
@@ -1,40 +1,21 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-class Queue {
-  constructor() {
-    this.queue = [];
-    for (let i = 0; i < N; i++) {
-      if (A[i] === 0) this.queue.push(B[i]);
-    }
-    this.front = 0;
-    this.rear = this.queue.length;
-  }
-
-  unshift(num) {
-    this.front--;
-    this.queue[this.front] = num;
-  }
-
-  pop() {
-    this.rear--;
-    const popNum = this.queue[this.rear];
-    delete this.queue[this.rear];
-    return popNum;
-  }
-}
-
 const N = Number(input[0]);
 const A = input[1].split(" ").map(Number);
 const B = input[2].split(" ").map(Number);
 const M = Number(input[3]);
 const C = input[4].split(" ").map(Number);
-const queue = new Queue();
-const result = [];
+const queue = [];
 
-for (let i = 0; i < M; i++) {
-  queue.unshift(C[i]);
-  result.push(queue.pop());
+for (let i = 0; i < N; i++) {
+  if (A[i] === 0) queue.push(B[i]);
 }
 
-console.log(result.join(" "));
+const reverseQueue = queue.reverse();
+
+for (let i = 0; i < M; i++) {
+  reverseQueue.push(C[i]);
+}
+
+console.log(reverseQueue.slice(0, M).join(" "));
