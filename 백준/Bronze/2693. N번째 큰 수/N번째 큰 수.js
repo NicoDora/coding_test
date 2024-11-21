6ac48f4d2e0b34aff4304bf2c971afafdbf0a56d
NicoDora <pjh_2004@naver.com>
2024-11-21 18:25:23 +0900

    [Bronze I] Title: N번째 큰 수, Time: 108 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.js
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
-const [T, ...input] = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
+const T = Number(input[0]);
 const result = [];
 
-for (let i = 0; i < T; i++) {
+for (let i = 1; i <= T; i++) {
   const A = input[i].split(" ").map(Number);
   result.push(A.sort((a, b) => b - a)[2]);
 }
