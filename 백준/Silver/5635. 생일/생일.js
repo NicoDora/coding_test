2addcf045a83b8bb04e24c3e0da7f7694b4d75a9
NicoDora <pjh_2004@naver.com>
2024-11-22 12:05:49 +0900

    [Silver V] Title: 생일, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+const n = Number(input[0]);
+const array = [];
+
+for (let i = 1; i <= n; i++) {
+  array.push(input[i].split(" "));
+}
+
+const sorted = array.sort((a, b) => a[3] - b[3] || a[2] - b[2] || a[1] - b[1]);
+
+console.log(sorted.pop()[0] + "\n" + sorted[0][0]);
