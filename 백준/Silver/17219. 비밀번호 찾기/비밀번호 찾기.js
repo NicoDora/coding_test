4e4ed0f055ac8e7de0de7cbaa6bdf854da608267
NicoDora <pjh_2004@naver.com>
2024-11-22 17:50:21 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 420 ms, Memory: 68248 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+const [N, M] = input[0].split(" ").map(Number);
+const map = new Map();
+const result = [];
+
+for (let i = 1; i <= N; i++) {
+  const [site, pw] = input[i].split(" ");
+  map.set(site, pw);
+}
+
+for (let i = N + 1; i <= M + N; i++) {
+  result.push(map.get(input[i].split(" ")[0]));
+}
+
+console.log(result.join("\n"));
