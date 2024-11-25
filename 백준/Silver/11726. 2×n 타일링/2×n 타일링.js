4aa97baf190e0bde9805dde4661c4f59644864e6
NicoDora <pjh_2004@naver.com>
2024-11-25 15:06:12 +0900

    [Silver III] Title: 2×n 타일링, Time: 148 ms, Memory: 9660 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const n = Number(fs.readFileSync(0, "utf-8").toString().trim());
 
-const array = new Array(1000).fill(0);
+const array = new Array(n).fill(0);
 
 array[0] = 1;
 array[1] = 1;
 
-for (let i = 2; i <= 1000; i++) {
+for (let i = 2; i <= n; i++) {
   array[i] = (array[i - 1] + array[i - 2]) % 10007;
 }
 
