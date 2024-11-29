79fa1909d9b2a6089b3c7fcbdc18413b35974acb
NicoDora <pjh_2004@naver.com>
2024-11-29 21:18:44 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 18112 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,20 +1,26 @@
 const fs = require("fs");
 const [N, M] = fs.readFileSync(0, "utf-8").toString().trim().split(" ").map(Number);
 
-function dfs(depth, array) {
+function dfs(depth) {
   if (depth === M) {
     result.push(array.join(" "));
     return;
   }
 
   for (let i = 1; i <= N; i++) {
-    if (!array.includes(i)) {
-      dfs(depth + 1, [...array, i]);
+    if (!visited[i]) {
+      visited[i] = true;
+      array.push(i);
+      dfs(depth + 1);
+      visited[i] = false;
+      array.pop();
     }
   }
 }
 
+const visited = [];
 const result = [];
+const array = [];
 
-dfs(0, []);
+dfs(0);
 console.log(result.join("\n"));
