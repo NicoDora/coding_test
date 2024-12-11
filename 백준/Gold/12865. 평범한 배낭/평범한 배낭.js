8cbb0fbdbeb90950348ef2869c8234a2d72c682b
NicoDora <pjh_2004@naver.com>
2024-12-11 16:53:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 216 ms, Memory: 10760 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -2,21 +2,18 @@ const fs = require("fs");
 const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
 const [N, K] = input[0].split(" ").map(Number);
-const dp = Array.from({ length: K + 1 }, () => Array(N + 1).fill(0));
-const objects = [[]];
+const dp = Array(K + 1).fill(0);
+const objects = [];
 
 // 물건에 대한 배열 [무게, 가치]
 for (let i = 1; i <= N; i++) {
   objects.push(input[i].split(" ").map(Number));
 }
 
-for (let j = 1; j <= N; j++) {
-  const [W, V] = objects[j];
-
-  for (let i = 1; i <= K; i++) {
-    if (W > i) dp[i][j] = dp[i][j - 1];
-    else dp[i][j] = Math.max(dp[i][j - 1], dp[i - W][j - 1] + V);
+for (const [W, V] of objects) {
+  for (let i = K; i >= W; i--) {
+    dp[i] = Math.max(dp[i], dp[i - W] + V);
   }
 }
 
-console.log(dp[K][N]);
+console.log(dp[K]);
