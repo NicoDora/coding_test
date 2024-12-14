47b1d2e1faaa13a4b27ddab94dd7ef256efffd32
NicoDora <pjh_2004@naver.com>
2024-12-14 11:49:36 +0900

    [Bronze II] Title: 우리 집 밑에 편의점이 있는데, Time: 96 ms, Memory: 9416 KB -BaekjoonHub

--- 백준/Bronze/20155. 우리 집 밑에 편의점이 있는데/우리 집 밑에 편의점이 있는데.js
+++ 백준/Bronze/20155. 우리 집 밑에 편의점이 있는데/우리 집 밑에 편의점이 있는데.js
@@ -3,10 +3,10 @@ const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
 const [N, M] = input[0].split(" ").map(Number);
 const X = input[1].split(" ").map(Number);
-const count = [];
+const count = Array(M + 1).fill(0);
 
-for (let i = 1; i <= M; i++) {
-  count.push(X.filter((e) => e === i).length);
+for (const num of X) {
+  count[num]++;
 }
 
 console.log(Math.max(...count));
