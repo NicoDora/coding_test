d2814161b684efe0b5c1bf222e9d8cf499f2c7f7
NicoDora <pjh_2004@naver.com>
2025-01-04 13:32:50 +0900

    [Silver V] Title: 욱제는 결정장애야!!, Time: 244 ms, Memory: 37148 KB -BaekjoonHub

--- 백준/Silver/14646. 욱제는 결정장애야！！/욱제는 결정장애야！！.js
+++ 백준/Silver/14646. 욱제는 결정장애야！！/욱제는 결정장애야！！.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
-const N = Number(input[0]);
 const menus = input[1].split(" ").map(Number);
 const set = new Set();
 let sticker = 0;
 let max = 0;
 
-for (let i = 0; i < N * 2; i++) {
+for (let i = 0; i < menus.length; i++) {
   const menu = menus[i];
 
   if (!set.has(menu)) {
