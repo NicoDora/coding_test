dc6beb4e7b25f628e7ebe6c569207936dd8d0b3c
NicoDora <pjh_2004@naver.com>
2025-01-06 22:49:28 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 404 ms, Memory: 46696 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.js
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.js
@@ -1,6 +1,52 @@
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
+function findRoot(node) {
+  if (parent[node] === node) return node;
+  return (parent[node] = findRoot(parent[node])); // root노드 탐색 및 경로 압축
+}
+
+function union(node1, node2) {
+  const root1 = findRoot(node1);
+  const root2 = findRoot(node2);
+
+  // 깊이(rank)가 더 낮은 트리를 높은 트리에 붙임
+  if (root1 !== root2) {
+    if (rank[root1] > rank[root2]) parent[root2] = root1;
+    else if (rank[root1] < rank[root2]) parent[root1] = root2;
+    else {
+      parent[root2] = root1;
+      rank[root1]++;
+    }
+    return true;
+  }
+  return false;
+}
+
+const [V, E] = input[0].split(" ").map(Number);
+const edges = input.slice(1).map((line) => line.split(" ").map(Number));
+const parent = Array.from({ length: V + 1 }, (_, i) => i);
+const rank = Array(V + 1).fill(0);
+let mstWeight = 0;
+let edgeCount = 0;
+
+edges.sort((a, b) => a[2] - b[2]);
+
+// 크루스칼(Kurskal) 알고리즘
+for (let i = 0; i < E; i++) {
+  const [a, b, weight] = edges[i];
+
+  if (union(a, b)) {
+    mstWeight += weight;
+    edgeCount++;
+
+    if (edgeCount === V - 1) break;
+  }
+}
+
+console.log(mstWeight);
+
+/* 프림(Prim) 알고리즘
 class MinHeap {
   constructor() {
     this.heap = [];
@@ -65,7 +111,7 @@ const graph = {};
 const minHeap = new MinHeap();
 const visited = Array(V + 1).fill(false);
 let mstWeight = 0;
-let line = 0;
+let edgeCount = 0;
 
 for (let i = 1; i <= V; i++) {
   graph[i] = [];
@@ -87,7 +133,7 @@ for (const [node, weight] of graph[1]) {
 }
 
 // MST
-while (line < V - 1) {
+while (edgeCount < V - 1) {
   // 최소 가중치의 간선 꺼내기
   const [node, weight] = minHeap.shiftMin();
 
@@ -96,7 +142,7 @@ while (line < V - 1) {
 
   visited[node] = true;
   mstWeight += weight;
-  line++;
+  edgeCount++;
 
   // 최소 가중치를 가진 간선의 노드를 minHeap에 추가
   for (const [nextNode, edgeWeight] of graph[node]) {
@@ -105,3 +151,4 @@ while (line < V - 1) {
 }
 
 console.log(mstWeight);
+*/
