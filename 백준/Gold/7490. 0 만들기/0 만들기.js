e35ca005c0a64e0846ab7cd90a74426021db35ed
NicoDora <pjh_2004@naver.com>
2025-03-25 20:05:54 +0900

    [Gold V] Title: 0 만들기, Time: 148 ms, Memory: 10908 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.js
+++ 백준/Gold/7490. 0 만들기/0 만들기.js
@@ -1,33 +1,30 @@
 const fs = require("fs");
 const [T, ...N] = fs.readFileSync(0, "utf-8").toString().trim().split("\n").map(Number);
 
-
 function dfs(n, depth, temp, calc, before) {
   if (n === depth) {
-    if (calc === 0) tempResult.push(temp);
+    if (calc === 0) result.push(temp);
     return;
   }
-
-  // '+' 연산
-  dfs(n, depth + 1, temp + "+" + (depth + 1), calc + (depth + 1), depth + 1);
-
-  // '-' 연산
-  dfs(n, depth + 1, temp + "-" + (depth + 1), calc - (depth + 1), -(depth + 1));
 
   // ' ' 연산
   let newBefore =
     before >= 0 ? before * 10 + (depth + 1) : before * 10 - (depth + 1);
   let newCalc = calc - before + newBefore;
   dfs(n, depth + 1, temp + " " + (depth + 1), newCalc, newBefore);
+
+  // '+' 연산
+  dfs(n, depth + 1, temp + "+" + (depth + 1), calc + (depth + 1), depth + 1);
+
+  // '-' 연산
+  dfs(n, depth + 1, temp + "-" + (depth + 1), calc - (depth + 1), -(depth + 1));
 }
 
 const result = [];
-let tempResult = [];
 
 for (let i = 0; i < T; i++) {
   dfs(N[i], 1, "1", 1, 1);
-  result.push(tempResult.sort());
-  tempResult = [];
+  result.push("");
 }
 
-console.log(result.map((res) => res.join("\n")).join("\n\n"));
+console.log(result.join("\n"));
