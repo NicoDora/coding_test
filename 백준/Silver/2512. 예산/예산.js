af0e60cd84cb9f5f9bfa4b4b80bbb340f1cbccfd
NicoDora <pjh_2004@naver.com>
2025-03-30 22:18:48 +0900

    [Silver II] Title: 예산, Time: 156 ms, Memory: 10180 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -1,19 +1,36 @@
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
+// function binarySearch(start, end) {
+//   if (start > end) return end;
+
+//   const mid = Math.floor((start + end) / 2);
+//   let sum = 0;
+
+//   for (let i = 0; i < N; i++) {
+//     if (mid < request[i]) sum += mid;
+//     else sum += request[i];
+//   }
+
+//   if (sum > total) return binarySearch(start, mid - 1);
+//   else return binarySearch(mid + 1, end);
+// }
+
 function binarySearch(start, end) {
-  if (start > end) return end;
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    let sum = 0;
 
-  const mid = Math.floor((start + end) / 2);
-  let sum = 0;
+    for (let i = 0; i < N; i++) {
+      if (mid < request[i]) sum += mid;
+      else sum += request[i];
+    }
 
-  for (let i = 0; i < N; i++) {
-    if (mid < request[i]) sum += mid;
-    else sum += request[i];
+    if (sum > total) end = mid - 1;
+    else start = mid + 1;
   }
 
-  if (sum > total) return binarySearch(start, mid - 1);
-  else return binarySearch(mid + 1, end);
+  return end;
 }
 
 const N = Number(input[0]);
