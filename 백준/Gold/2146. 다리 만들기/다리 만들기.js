07caeea774668f5bafe61138f893fdd0324edd19
NicoDora <pjh_2004@naver.com>
2025-04-16 17:28:29 +0900

    [Gold III] Title: 다리 만들기, Time: 200 ms, Memory: 18592 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.js
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.js
@@ -54,51 +54,21 @@ function floodFill(i, j, id) {
       const nx = x + dx;
       const ny = y + dy;
 
-      if (nx >= 0 && nx < N && ny >= 0 && ny < N && map[nx][ny] === 1) {
-        queue.push([nx, ny]);
-        map[nx][ny] = id;
-      }
-    }
-  }
-}
-
-function findShortestBridge(startId) {
-  const queue = new Queue();
-  const distance = Array.from({ length: N }, () => Array(N).fill(-1));
-
-  // 시작 섬을 큐에 추가
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (map[i][j] === startId) {
-        queue.push([i, j]);
-        distance[i][j] = 0;
-      }
-    }
-  }
-
-  while (queue.size) {
-    const [x, y] = queue.pop();
-
-    for (const [dx, dy] of directions) {
-      const nx = x + dx;
-      const ny = y + dy;
-
       if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 
-      // 아직 방문하지 않은 바다인 경우
-      if (distance[nx][ny] === -1 && map[nx][ny] === 0) {
-        distance[nx][ny] = distance[x][y] + 1;
+      if (map[nx][ny] === 1) {
         queue.push([nx, ny]);
-        // 다른 섬에 도달한 경우
-      } else if (map[nx][ny] !== 0 && map[nx][ny] !== startId) {
-        return distance[x][y];
-      }
+        map[nx][ny] = id;
+      } else if (map[nx][ny] === 0) edge.push([x, y, id]);
     }
   }
 }
 
 const N = Number(input[0]);
 const map = input.slice(1).map((row) => row.split(" ").map(Number));
+const visited = Array.from({ length: N }, () => Array(N).fill(false));
+const distance = Array.from({ length: N }, () => Array(N).fill(0));
+const edge = new Queue();
 const directions = [
   [0, 1],
   [1, 0],
@@ -111,13 +81,30 @@ let result = Infinity;
 // 섬 구분
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    if (map[i][j] === 1) floodFill(i, j, islandId++);
+    if (map[i][j] === 1 && !visited[i][j]) floodFill(i, j, islandId++);
   }
 }
 
-// 각 섬에서 설치 가능한 다리의 최소 길이 구하기
-for (let id = 2; id < islandId; id++) {
-  result = Math.min(result, findShortestBridge(id));
+// 각 섬의 edge에서 설치 가능한 다리의 최소 길이 구하기
+while (edge.size) {
+  const [x, y, id] = edge.pop();
+
+  for (const [dx, dy] of directions) {
+    const nx = x + dx;
+    const ny = y + dy;
+
+    if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+
+    // 바다인 경우
+    if (map[nx][ny] === 0) {
+      edge.push([nx, ny, id]);
+      map[nx][ny] = id;
+      distance[nx][ny] = distance[x][y] + 1;
+      // 다른 섬에 도달한 경우
+    } else if (map[nx][ny] !== id) {
+      result = Math.min(result, distance[nx][ny] + distance[x][y]);
+    }
+  }
 }
 
 console.log(result);
