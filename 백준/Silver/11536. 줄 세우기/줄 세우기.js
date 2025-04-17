1c036d863e742cb967341a375fa6ea42221aa23f
NicoDora <pjh_2004@naver.com>
2025-04-18 00:00:24 +0900

    [Silver V] Title: 줄 세우기, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Silver/11536. 줄 세우기/줄 세우기.js
+++ 백준/Silver/11536. 줄 세우기/줄 세우기.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
-const N = Number(input[0]);
-const names = input.slice(1);
-const asc = [...names].sort();
-const desc = [...names].sort().reverse();
-
 function arraysEqual(a, b) {
   return a.every((v, i) => v === b[i]);
 }
 
-if (arraysEqual(names, asc)) console.log("INCREASING");
-else if (arraysEqual(names, desc)) console.log("DECREASING");
-else console.log("NEITHER");
+const names = input.slice(1);
+const asc = [...names].sort();
+const desc = [...names].sort().reverse();
+
+console.log(
+  arraysEqual(names, asc)
+    ? "INCREASING"
+    : arraysEqual(names, desc)
+    ? "DECREASING"
+    : "NEITHER"
+);
