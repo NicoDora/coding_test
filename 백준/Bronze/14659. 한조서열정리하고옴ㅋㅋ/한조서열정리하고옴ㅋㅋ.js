09623411689251b99f0c74ad9bb9591fc329a577
NicoDora <pjh_2004@naver.com>
2025-04-26 22:45:12 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 164 ms, Memory: 12540 KB -BaekjoonHub

---
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+const N = Number(input[0]);
+const array = input[1].split(" ").map(Number);
+const nextGreaterIndex = Array(N).fill(N);
+const stack = [];
+let result = 0;
+
+for (let i = N - 1; i >= 0; i--) {
+  const h = array[i];
+
+  while (stack.length && stack[stack.length - 1][0] <= h) stack.pop();
+
+  if (stack.length) nextGreaterIndex[i] = stack[stack.length - 1][1];
+
+  stack.push([h, i]);
+}
+
+for (let i = 0; i < N; i++) {
+  const kills = nextGreaterIndex[i] - i - 1;
+  if (kills > result) result = kills;
+}
+
+console.log(result);
