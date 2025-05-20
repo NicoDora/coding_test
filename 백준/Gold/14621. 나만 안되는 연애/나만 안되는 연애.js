93d0189a2c9837bc22661c6f5166ae20149808b8
NicoDora <pjh_2004@naver.com>
2025-05-20 18:05:50 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 184 ms, Memory: 15284 KB -BaekjoonHub

---
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+function findRoot(node) {
+  if (parent[node] === node) return node;
+  return (parent[node] = findRoot(parent[node]));
+}
+
+function union(node1, node2) {
+  const root1 = findRoot(node1);
+  const root2 = findRoot(node2);
+
+  if (root1 === root2) return false;
+
+  if (rank[root1] > rank[root2]) parent[root2] = root1;
+  else {
+    parent[root1] = root2;
+    if (rank[root1] === rank[root2]) rank[root2]++;
+  }
+
+  return true;
+}
+
+const [N, M] = input[0].split(" ").map(Number);
+const gender = [0, ...input[1].split(" ")];
+const edges = input.slice(2).map((row) => row.split(" ").map(Number));
+const parent = Array.from({ length: N + 1 }, (_, i) => i);
+const rank = Array(N + 1).fill(0);
+let mstWeight = 0;
+let mstEdgeCount = 0;
+
+edges.sort((a, b) => a[2] - b[2]);
+
+for (const [node1, node2, weight] of edges) {
+  if (gender[node1] !== gender[node2] && union(node1, node2)) {
+    mstWeight += weight;
+    mstEdgeCount++;
+
+    if (mstEdgeCount === N - 1) break;
+  }
+}
+
+console.log(mstEdgeCount === N - 1 ? mstWeight : -1);
