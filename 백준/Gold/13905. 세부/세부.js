d38d51c20aca9fae6641e4e56a276832f56959ce
NicoDora <pjh_2004@naver.com>
2025-05-27 17:07:12 +0900

    [Gold III] Title: 세부, Time: 704 ms, Memory: 107144 KB -BaekjoonHub

---
+++ 백준/Gold/13905. 세부/세부.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+function findRoot(node) {
+  if (parent[node] === node) return node;
+  return (parent[node] = findRoot(parent[node]));
+}
+
+function union(node1, node2) {
+  const root1 = findRoot(node1);
+  const root2 = findRoot(node2);
+
+  if (root1 === root2) return false;
+
+  if (rank[root1] > rank[root2]) parent[root2] = root1;
+  else {
+    parent[root1] = root2;
+    if (rank[root1] === rank[root2]) rank[root2]++;
+  }
+
+  return true;
+}
+
+const [N, M] = input[0].split(" ").map(Number);
+const [s, e] = input[1].split(" ").map(Number);
+const edges = input.slice(2).map((row) => row.split(" ").map(Number));
+const parent = Array.from({ length: N + 1 }, (_, i) => i);
+const rank = Array(N + 1).fill(0);
+let result = 0;
+
+edges.sort((a, b) => b[2] - a[2]);
+
+for (const [node1, node2, weight] of edges) {
+  if (union(node1, node2) && findRoot(s) === findRoot(e)) {
+    result = weight;
+    break;
+  }
+}
+
+console.log(result);
